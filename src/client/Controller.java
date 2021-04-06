e0d6b47649a124cac7a6aca4b1510f469a70dab6
itpark <itpark.team@gmail.com>
2021-04-06 19:04:28 +0300

    draw field

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.TextField;
 
 public class Controller {
 
@@ -28,9 +27,27 @@ public class Controller {
         double w = canvasField.getWidth();
         double h = canvasField.getHeight();
 
+        //line x 1
         gc.moveTo(0, dy);
         gc.lineTo(w, dy);
         gc.stroke();
+
+        //line x 2
+        gc.moveTo(0, dy*2);
+        gc.lineTo(w, dy*2);
+        gc.stroke();
+
+        //line y 1
+        gc.moveTo(dx, 0);
+        gc.lineTo(dx, h);
+        gc.stroke();
+
+        //line y 2
+        gc.moveTo(dx*2, 0);
+        gc.lineTo(dx*2, h);
+        gc.stroke();
+
+
     }
 
     public void btnConnectClick(ActionEvent actionEvent) {
